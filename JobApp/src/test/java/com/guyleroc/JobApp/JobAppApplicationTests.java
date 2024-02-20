16b52d9101705d89cb90c29cc91eca95d69e7de1
Guy-leroc <thonerguyleroc1@gmail.com>
2024-02-20 10:07:01 -0500

    Inital commit job app

---
+++ JobApp/src/test/java/com/guyleroc/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyleroc.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
